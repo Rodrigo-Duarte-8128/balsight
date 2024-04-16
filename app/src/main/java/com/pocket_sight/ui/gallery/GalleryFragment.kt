cf448ebdaef6accf9d12d3b2bc1496dd9e5d39e8
Rodrigo-Duarte-8128 <rodrigolealduarte@hotmail.com>
2024-04-16 15:19:55 +0100

    add elements to home screen: text views for accout selected, month displayed and the headers data, category and value

--- app/src/main/java/com/pocket_sight/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/pocket_sight/ui/gallery/GalleryFragment.kt
@@ -29,9 +29,10 @@ class GalleryFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        textView.text = "some other text"
+        //galleryViewModel.text.observe(viewLifecycleOwner) {
+        //    textView.text = it
+        //}
         return root
     }
 
