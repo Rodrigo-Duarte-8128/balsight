336fb99f0041eb45cd1bac961898cb4be69329f2
Rodrigo Duarte <rodrigolealduarte@hotmail.com>
2024-04-19 14:11:11 +0100

    Create Room annotations for Account data class.

--- app/src/main/java/com/pocket_sight/types/Account.kt
+++ app/src/main/java/com/pocket_sight/types/Account.kt
@@ -1,9 +1,21 @@
 package com.pocket_sight.types
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "accounts_table")
 data class Account(
+    @PrimaryKey
     val number: Int,
+
+    @ColumnInfo(name = "account_name")
     var name: String,
+
+    @ColumnInfo(name="account_balance")
     var balance: Double,
+    
+    @ColumnInfo(name="account_balance")
     var current: Boolean = false
 )
 
